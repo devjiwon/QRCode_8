cd8d8a783c923065420ada16ddd0c91eddfb2a24
dpwls2215 <dpwls2215@naver.com>
2018-12-02 09:30:11 +0900

    divide mode_test modification

--- src/qrcode.c
+++ src/qrcode.c
@@ -34,6 +34,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+bool isAlphanumeric(const char *text, uint16_t length);
+bool isNumeric(const char *text, uint16_t length);
+
 #pragma mark - Error Correction Lookup tables
 
 #if LOCK_VERSION == 0
@@ -97,43 +101,6 @@ static int abs(int value) {
 */
 
 
-#pragma mark - Mode testing and conversion
-
-static int8_t getAlphanumeric(char c) {
-    
-    if (c >= '0' && c <= '9') { return (c - '0'); }
-    if (c >= 'A' && c <= 'Z') { return (c - 'A' + 10); }
-    
-    switch (c) {
-        case ' ': return 36;
-        case '$': return 37;
-        case '%': return 38;
-        case '*': return 39;
-        case '+': return 40;
-        case '-': return 41;
-        case '.': return 42;
-        case '/': return 43;
-        case ':': return 44;
-    }
-    
-    return -1;
-}
-
-static bool isAlphanumeric(const char *text, uint16_t length) {
-    while (length != 0) {
-        if (getAlphanumeric(text[--length]) == -1) { return false; }
-    }
-    return true;
-}
-
-
-static bool isNumeric(const char *text, uint16_t length) {
-    while (length != 0) {
-        char c = text[--length];
-        if (c < '0' || c > '9') { return false; }
-    }
-    return true;
-}
 
 
 #pragma mark - Counting
